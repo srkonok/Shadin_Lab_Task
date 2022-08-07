bf140938188e8c667512a2ddb049dd4ed9293cac
Shahariar R. Konok <srkonok20@gmail.com>
2022-08-07 13:02:34 +0600

    Update Signin.py

--- Task3 Added Password Hashing and JWT/Signin.py
+++ Task3 Added Password Hashing and JWT/Signin.py
@@ -3,6 +3,7 @@ import mycon as mc
 import datetime,time
 import hashlib,hmac
 import jwt
+from pydantic import ValidationError
 from ParserUser import User
 
 def lambda_handler(event, context):
@@ -12,36 +13,37 @@ def lambda_handler(event, context):
 
         body = json.loads(event['body'])
         
-
         try :
             data=User(**body)
             this_email = data.email
             this_password = data.password
-        except ValidationError as e:
-            print(e.json())
-      
-        hash_pass=hashlib.md5(this_password.encode('utf-8')).hexdigest()
-        print(hash_pass)
+            
+            hash_pass=hashlib.md5(this_password.encode('utf-8')).hexdigest()
+            print(hash_pass)
         
-        mycursor = dbmy.cursor()
-        mycursor.execute("select * from user where email = %s",(this_email,))
-        user = mycursor.fetchone()
+            mycursor = dbmy.cursor()
+            mycursor.execute("select * from user where email = %s",(this_email,))
+            user = mycursor.fetchone()
         
-        if user:
-            email = user[1]
-            password = user[2]
-            print(password)
-            if hash_pass == password:
-                token=jwt.encode({'Auth':'Allow',"exp": datetime.datetime.utcnow()+ datetime.timedelta(seconds=2)}, "secret", algorithm="HS256")
-                mycursor.execute('insert into Token (email, token) values(%s, %s)', (email,token,))
-                dbmy.commit()
-                message = "Successfully Sign in and Token Generated!!"
+            if user:
+                email = user[1]
+                password = user[2]
+                print(password)
+                if hash_pass == password:
+                    token=jwt.encode({'Auth':'Allow',"exp": datetime.datetime.utcnow()+ datetime.timedelta(hours=2)}, "secret", algorithm="HS256")
+                    mycursor.execute('insert into Token (email, token) values(%s, %s)', (email,token,))
+                    dbmy.commit()
+                    message = "Successfully Sign in and Token Generated!!"
+                else:
+                    message = "Invalid Password!!"
             else:
-                message = "Invalid Password!!"
-                 
-        else:
-            message = "User's not registered!!"
-        
+                message = "User's not registered!!"
+            
+        except ValidationError as e:
+            jsonf=e.json()
+            print(jsonf)
+            message=jsonf
+      
     except :
         message = "Can't signin!!"
     
